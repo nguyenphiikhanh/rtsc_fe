aa078014a660ac3ef7d68b8fe9f96db04485b8b8
khanhnt <nguyenphiikhanh@gmail.com>
2025-12-17 16:17:24 +0700

    setup tabs layout

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import {TabsUIComponentResolver} from 'vue3-tailwind-tabs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [
+        vue(),
+        Components({
+            resolvers: [TabsUIComponentResolver()],
+        })
+    ],
 })
