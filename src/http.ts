414fac56a952b4cd84783f9c0fc4eb162b4ad7db
khanhnt <nguyenphiikhanh@gmail.com>
2025-12-18 02:48:23 +0700

    fix: deploy vercel config

--- src/http.ts
+++ src/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: '/api',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: false
 });
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-import {defineConfig, loadEnv} from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import {TabsUIComponentResolver} from 'vue3-tailwind-tabs'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({mode}) => {
-    const env = loadEnv(mode, process.cwd());
-
+export default defineConfig(({}) => {
     return {
         plugins: [
             vue(),
@@ -15,15 +13,6 @@ export default defineConfig(({mode}) => {
             Components({
                 resolvers: [TabsUIComponentResolver()],
             })
-        ],
-        server: {
-            proxy: {
-                '/api': {
-                    target: env.VITE_API_BASE_URL,
-                    changeOrigin: true,
-                    secure: false,
-                }
-            }
-        }
+        ]
     }
 })
